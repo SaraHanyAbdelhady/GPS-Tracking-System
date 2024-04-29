45a157482cfb8d943ed99034c5f22ec130770f5d
Marina-Mokhtar <2100929@eng.asu.edu.eg>
2024-04-30 00:44:36 +0200

    Update GPS.c

--- src/HAL/GPS/GPS.c
+++ src/HAL/GPS/GPS.c
@@ -68,41 +68,43 @@ void GPS_voidReceiveSentence(f64* Copy_f64Latitude, f64 *Copy_f64Longitude, u8 *
 		}
 	} while (Local_u8ReadCounter == 0);
 }
-void GPS_voidExtractCoordinates(u8 *Copy_pu8Sentence,f64 *Copy_f64Latitude,f64 *Copy_f64Longitude, u8 *Copy_u8Speed){
-	f32 Local_f32Deg,Local_f32Min, Local_f32Sec;
-	u8 Local_u8CommaCount =0;
-	u8 j=0;
-	u8 k =0;
-	u8 l=0;
-	char longARR[15]={0};
+
+void GPS_voidExtractCoordinates(u8 *Copy_pu8sentence,f64 *Copy_f64Latitude,f64 *Copy_f64Longitude,u8 *Copy_u8Speed)
+{
+	f32 Local_f32Deg,Local_f32Min,Local_f32Sec;
 	char altitudeARR[15]={0};
+	char longARR[15]={0};
+	char SpeedARR[10];
+	u8 j=0;
+	u8 k=0;
+	u8 l=0;
 	u8 i=0;
-	char speedArr[10];
-	while (Local_u8CommaCount <7)
+	u8 Local_u8_CommaCount =0;
+	while (Local_u8_CommaCount<7)
 	{
-		if (Copy_pu8Sentence[i]==',')
+		if (Copy_pu8sentence[i]==',')
 		{
-			Local_u8CommaCount++;
+			Local_u8_CommaCount++;
 			i++;
 		}
-		if (Local_u8CommaCount==1)
+		if (Local_u8_CommaCount==1)
 		{
-			if (Copy_pu8Sentence[i]!='A')
+			if (Copy_pu8sentence[i]!='A')
 			{
-				GPS_voidReceiveSentence(Copy_f64Latitude,Copy_f64Longitude,Copy_u8Speed);
+				GPS_voidReceiveSentence(Copy_f64Latitude, Copy_f64Longitude,Copy_u8Speed);
 			}
 		}
-		if(Local_u8CommaCount==2)
+	    if (Local_u8_CommaCount==2)
 		{
-			altitudeARR[j++]=Copy_pu8Sentence[i];
+           altitudeARR[j++]=Copy_pu8sentence[i];
 		}
-		if(Local_u8CommaCount==4)
+	    if (Local_u8_CommaCount==4)
 		{
-			longARR[k++]=Copy_pu8Sentence[i];
+           longARR[k++]=Copy_pu8sentence[i];
 		}
-		if(Local_u8CommaCount==6)
+		if (Local_u8_CommaCount==6)
 		{
-			speedArr[l++]=Copy_pu8Sentence[i];
+           SpeedARR[l++]=Copy_pu8sentence[i];
 		}
 		i++;
 	}
@@ -110,21 +112,20 @@ void GPS_voidExtractCoordinates(u8 *Copy_pu8Sentence,f64 *Copy_f64Latitude,f64 *
 	Local_f32Deg=*Copy_f64Latitude/100;
 	Local_f32Min=*Copy_f64Latitude-(f32)(Local_f32Deg*100);
 	Local_f32Sec=Local_f32Min/60.0;
-	*Copy_f64Latitude=Local_f32Deg + Local_f32Sec;
+	*Copy_f64Latitude= Local_f32Deg+Local_f32Sec;
 
 
 	*Copy_f64Longitude=atof(longARR);
 	Local_f32Deg=*Copy_f64Longitude/100;
 	Local_f32Min=*Copy_f64Longitude-(f32)(Local_f32Deg*100);
 	Local_f32Sec=Local_f32Min/60.0;
-	*Copy_f64Longitude=Local_f32Deg + Local_f32Sec;
-
-	for(int i =0;i<4;i++)
+	*Copy_f64Longitude= Local_f32Deg+Local_f32Sec;
+	for (int i =0;i<4;i++)
 	{
-		GPS_u8SpeedArr[i]=speedArr[i];
+		GPS_u8SpeedArr[i]=SpeedARR[i];
 	}
-    speedArr[i]='\0';
-	*Copy_u8Speed=atof(speedArr);
+    SpeedARR[i]='\0';
+	*Copy_u8Speed=atof(SpeedARR);
 }
 
 
