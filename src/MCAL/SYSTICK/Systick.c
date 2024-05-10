01e14325eed62b5ce2caf9c8b603dc56cbcf41da
SaraHanyAbdelhady <160270735+SaraHanyAbdelhady@users.noreply.github.com>
2024-05-10 21:15:20 +0300

    troubling for errors

--- src/MCAL/SYSTICK/Systick.c
+++ src/MCAL/SYSTICK/Systick.c
@@ -13,7 +13,7 @@ void Systick_Wait(u32 delay){
     NVIC_ST_RELOAD_R=delay-1;
     NVIC_ST_CURRENT_R=0;
 		NVIC_ST_CTRL_R=0x00000005;
-    while((NVIC_ST_CTRL_R&0x00010000)==0);  
+    //while((NVIC_ST_CTRL_R&0x00010000)==0);  
 }
 
 void delay (u32 delay){
