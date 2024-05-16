3eeb5115113a871efbe7cb1e69e4a6a09f52a132
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-05-16 20:52:19 +0300

    Update GPS.h

--- Headers/HAL/GPS/GPS.h
+++ Headers/HAL/GPS/GPS.h
@@ -8,8 +8,8 @@
 #define M_PI            3.1415926
 
 
-extern void GPS_voidReceiveSentence(f32 *copy_f32Latitude,f32 *copy_f32Longitude, u8 *copy_u8Speed);
 
-extern void GPS_voidExtractCoordinates(u8 *copy_pu8Sentence,f32 *copy_f32Latitude,f32*copy_f32Longitude, u8 *copy_u8Speed);
+void GPS_read(void);
+void GPS_format(void);
 
 #endif
